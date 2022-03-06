59d1d1799f17f37a6d679873fe8e924afb4ae14d
Maxime JUND <maxime.jund@gmail.com>
2022-03-06 23:54:52 +0100

    Fix wrong text vars.

--- resources/lang/fr/message.php
+++ resources/lang/fr/message.php
@@ -3,22 +3,19 @@
 return [
     'dashboard' => 'Tableau de bord',
     'welcome' => 'Bonjour ',
-    'loggedIn' => 'Vous êtes connecté !',
-    'logIn' => 'Connexion',
-    'logOut' => 'Déconnexion',
     'usersManagement' => 'Utilisateurs',
     'rolesManagement' => 'Rôles',
     'roles' => 'Rôles',
     'permissions' => 'Autorisations',
-    'email' => 'E-mail',
-    'password' => 'Mot de passe',
-    'password-confirm' => 'Confimer mot de passe',
-
+    
     'name' => 'Nom',
     'add' => 'Ajouter',
     'edit' => 'Modifier',
     'remove' => 'Supprimer',
     'save' => 'Enregistrer',
     'action' => 'Actions',
-    'back' => '< Précédent'
+    'back' => '< Précédent',
+    'whoops' => 'Oups !',
+    'whoopsDetails' => 'Il y a eu un problème avec votre saisie.',
+    'whoopsFull' => 'Oups ! Il y a un problème.',
 ];
