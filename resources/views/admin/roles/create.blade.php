10947e3523f821712fee151fe48ded2c157d9dba
Maxime JUND <maxime.jund@gmail.com>
2022-03-11 19:47:46 +0100

    Fix multiple design issues.

--- resources/views/admin/roles/create.blade.php
+++ resources/views/admin/roles/create.blade.php
@@ -2,12 +2,12 @@
     <x-content-page>
         <x-slot name="header">{{ __('message.rolesManagement') }}</x-slot>
         <x-slot name="headerSubtitle">
-            <a href="{{ route('roles.index') }}">
+            <a href="{{ url()->previous(); }}">
                 <i class="fa-solid fa-circle-chevron-left"></i> {{ __('message.back') }}
             </a>
         </x-slot>
 
-        {!! Form::open(['route' => 'roles.store', 'method' => 'POST', 'class' => 'box']) !!}
+        {!! Form::open(['route' => 'roles.store', 'method' => 'POST', 'class' => 'box-no-shadow']) !!}
         <div class="field">
             <label class="label">{{ __('message.name') }}</label>
             <div class="control">
@@ -35,7 +35,7 @@
 
         @can('role-mngt')
             <div class="field is-pulled-right pt-4">
-                <div class="control">
+                <div class="control is-pulled-right">
                     {!! Form::button('<span class="icon is-small"><i class="fa-solid fa-circle-plus"></i></span><span>' . __('message.save') . '</span>', ['class' => 'button is-success', 'type' => 'submit', 'name' => 'action', 'value' => 'save']) !!}
                 </div>
             </div>
